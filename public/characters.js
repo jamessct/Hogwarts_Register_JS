b166e8c0b1e5371724232e78ed420e41f3a9503f
James Scott <jamessct@hotmail.co.uk>
2016-10-02 20:56:57 +0100

    well i guess thats sort of progress..... i guess

--- public/characters.js
+++ public/characters.js
@@ -16,9 +16,26 @@ var returnAllCharacters = function() {
 var charactersByHouse = function(houses) {
   var character = characters[0];
   console.log(character.name)
-  var pTag = document.querySelector('#names')
   characters.forEach(function(character) {
     if(houses === character.house)
-      pTag.innerText = "Name: " + character.name + "\n";
+      // pTag.innerText = "Name: " + character.name + "\n";
+      var name = character.name;
+      addListItem(name);
   })
+}
+
+var createListItem = function(name) {
+  var listItem = document.createElement('li');
+  listItem.innerText = name;
+  return listItem;
+}
+
+var appendElements = function(listItem, ul) {
+  ul.appendChild(listItem);
+}
+
+var addListItem = function(name) {
+  var listItem = createListItem(name);
+  var ul = document.querySelector('#names')
+  appendElements(listItem, ul);
 }
